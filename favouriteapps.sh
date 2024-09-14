bf23dbc3b4af7b1dd9a77a38ee6bffde0aa9d7ce
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-09-14 17:47:07 +0200

    Update favouriteapps.sh

--- favouriteapps.sh
+++ favouriteapps.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+# flatpak install -assumeyes flathub it.mijorus.gearlever org.qbittorrent.qBittorrent org.gimp.GIMP org.kde.kdenlive org.nicotine_plus.Nicotine 
+# snap install vlc keepassxc
+#wget https://cdn.filen.io/desktop/release/filen_x86_64.AppImage
+#mkdir $HOME/AppImages/
+#cp filen_x86_64.AppImage $HOME/AppImages/filen.appimage
+#rm filen_x86_64.AppImage
+chmod +x $HOME/AppImages/filen.appimage
+
+wget https://github.com/Tsu-gu/tsubuntu/blob/main/filen.png
+mkdir $HOME/AppImages/.icons/
+mv filen.png $HOME/AppImages/.icons/
+
 wget https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/filen.desktop
 awk -v home="$HOME" '{gsub("/home/tsugu", home); print}' filen.desktop > new_filen.desktop
 cp new_filen.desktop $HOME/.local/share/applications/filen.desktop
