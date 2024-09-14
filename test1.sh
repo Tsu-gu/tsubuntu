bd5e4450c16cb50c8570497f92b066c1376cc125
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-09-14 08:38:44 +0200

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -1,23 +1,29 @@
 #!/bin/bash
-UBUNTU_CODENAME=$(cat /etc/*-release | grep UBUNTU_CODENAME | cut -d= -f2)
-if [[ "$UBUNTU_CODENAME" == "jammy" ]]; then
-    echo "Ubuntu Jammy detected. Installing tiling extension."
-    # ---the first extension---
-echo "Installing corner tiling extension..."
-downloadedzip="tiling-assistantleleat-on-github.v36.shell-extension.zip"
-linktozip="https://extensions.gnome.org/extension-data/tiling-assistantleleat-on-github.v36.shell-extension.zip"
-folder="tiling-assistant@leleat-on-github"
 
-mkdir $folder
-cd $folder
-wget $linktozip
-unzip $downloadedzip
-rm $downloadedzip
-cd ..
-mv $folder $HOME/.local/share/gnome-shell/extensions/
-if [[ "$UBUNTU_CODENAME" == "noble" ]]; then
-    echo "Ubuntu 24 doesn't need a tiling extension. One is already built in."
+# Extract Ubuntu codename
+UBUNTU_CODENAME=$(cat /etc/*-release | grep UBUNTU_CODENAME | cut -d= -f2 | tr -d '\r')
+
+# Check for Ubuntu codename
+if [[ "$UBUNTU_CODENAME" == "jammy" ]]; then
+  echo "Ubuntu Jammy detected. Installing tiling extension."
+
+  # ---the first extension---
+  echo "Installing corner tiling extension..."
+  downloadedzip="tiling-assistantleleat-on-github.v36.shell-extension.zip"
+  linktozip="https://extensions.gnome.org/extension-data/tiling-assistantleleat-on-github.v36.shell-extension.zip"
+  folder="tiling-assistant@leleat-on-github"
+
+  # Use quotes for variable expansions and commands
+  mkdir "$folder"
+  cd "$folder"
+  wget "$linktozip"
+  unzip "$downloadedzip"
+  rm "$downloadedzip"
+  cd ..
+  mv "$folder" "$HOME/.local/share/gnome-shell/extensions/"
+
+elif [[ "$UBUNTU_CODENAME" == "noble" ]]; then
+  echo "Ubuntu 24 (Noble) detected. Tiling extension is already built in."
 else
-   # CODE
-    
+  echo "Ubuntu $UBUNTU_CODENAME detected. Tiling extension not needed or specific instructions required."
 fi
