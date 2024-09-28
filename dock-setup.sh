43cf71fbcda2a6539a024b9d8200ebe9fdd78576
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-09-28 20:07:46 +0200

    Update dock-setup.sh

--- dock-setup.sh
+++ dock-setup.sh
@@ -1,44 +1,52 @@
 #!/bin/bash
 dconf write /org/gnome/shell/extensions/dash-to-dock/extend-height true
 sleep 1
-echo "Applying setting 1"
+echo "--------------------------------------------"
+echo "Extending the dock to the edges"
+echo "--------------------------------------------"
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'LEFT'"
 sleep 1
-echo "Applying setting 2"
+echo "--------------------------------------------"
+echo "Moving the dock to the left"
+echo "--------------------------------------------"
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed true
 sleep 1
-echo "Applying setting 3"
+echo "--------------------------------------------"
+echo "Disabling auto-hiding"
+echo "--------------------------------------------"
 dconf write /org/gnome/shell/extensions/dash-to-dock/click-action "'minimize-or-previews'"
 sleep 1
-echo "Applying setting 4"
+echo "--------------------------------------------"
+echo "Applying clicking on an icon to minimize"
+echo "--------------------------------------------"
 dconf write /org/gnome/shell/extensions/dash-to-dock/scroll-action "'switch-workspace'"
 sleep 1
-echo "Applying setting 5"
+echo "--------------------------------------------"
+echo "Applying switch workspace by scrolling"
+echo "--------------------------------------------"
 dconf write /org/gnome/shell/extensions/dash-to-dock/middle-click-action "'quit'"
 sleep 1
-echo "Applying setting 6"
+echo "--------------------------------------------"
+echo "Applying middle click action"
+echo "--------------------------------------------"
 dconf write /org/gnome/shell/extensions/dash-to-dock/custom-theme-shrink true
 sleep 1
-echo "Applying setting 7"
-
+echo "--------------------------------------------"
+echo "Removing the extra padding"
+echo "--------------------------------------------"
 dconf write /org/gnome/shell/extensions/dash-to-dock/background-opacity '0.80000000000000002'
 sleep 1
-echo "Applying setting 8"
+echo "--------------------------------------------"
+echo "Applying background opacity"
+echo "--------------------------------------------"
 
+# Re-running the same set of commands. This gave me a lot of trouble so I want to ensure everything works fine.
 dconf write /org/gnome/shell/extensions/dash-to-dock/extend-height true
-sleep 1
-echo "Re-running the same set of commands. This gave me a lot of trouble so I want to ensure everything works fine."
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'LEFT'"
-sleep 1
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed true
-sleep 1
 dconf write /org/gnome/shell/extensions/dash-to-dock/click-action "'minimize-or-previews'"
-sleep 1
 dconf write /org/gnome/shell/extensions/dash-to-dock/scroll-action "'switch-workspace'"
-sleep 1
 dconf write /org/gnome/shell/extensions/dash-to-dock/middle-click-action "'quit'"
-sleep 1
 dconf write /org/gnome/shell/extensions/dash-to-dock/custom-theme-shrink true
-sleep 1
 dconf write /org/gnome/shell/extensions/dash-to-dock/background-opacity '0.80000000000000002'
 echo "Done."
