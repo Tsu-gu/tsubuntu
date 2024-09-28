efc1a772e09dc29a0d37146f9071f814492ff5e2
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-09-28 16:23:26 +0200

    Update dock-setup.sh

--- dock-setup.sh
+++ dock-setup.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 dconf write /org/gnome/shell/extensions/dash-to-dock/extend-height true
 sleep 1
+echo "Applying setting 1"
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'LEFT'"
 sleep 1
+echo "Applying setting 2"
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed true
 sleep 1
+echo "Applying setting 3"
 dconf write /org/gnome/shell/extensions/dash-to-dock/click-action "'minimize-or-previews'"
 sleep 1
+echo "Applying setting 4"
 dconf write /org/gnome/shell/extensions/dash-to-dock/scroll-action "'switch-workspace'"
 sleep 1
+echo "Applying setting 5"
 dconf write /org/gnome/shell/extensions/dash-to-dock/middle-click-action "'quit'"
 sleep 1
+echo "Applying setting 6"
 
 dconf write /org/gnome/shell/extensions/dash-to-dock/extend-height true
 sleep 1
+echo "Re-running the same set of commands. This gave me a lot of trouble so I want to ensure everything works fine."
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-position "'LEFT'"
 sleep 1
 dconf write /org/gnome/shell/extensions/dash-to-dock/dock-fixed true
