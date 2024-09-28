1722e22a24f0aa953c27d4893bd981493efb820f
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-09-28 14:30:21 +0200

    Update and rename tsubuntudebian to tsubuntudebian.sh

--- tsubuntudebian.sh
+++ tsubuntudebian.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-sudo apt install gnome-tweaks flatpak unzip gdebi gnome-extensions-app dconf-editor libfuse2 -y
+sudo apt install gnome-tweaks flatpak unzip gdebi gnome-extensions-app dconf-editor libfuse2 snapd -y
 # libfuse2 in order for all AppImages to run
+sudo systemctl enable --now snapd.socket
+# for classic snaps to run
+sudo ln -s /var/lib/snapd/snap /snap
+
 echo "--------------------------------------------"
 echo "Adding Flathub..."
 echo "--------------------------------------------"
@@ -20,24 +24,24 @@ echo "--------------------------------------------"
 echo "Installing clipboard management extension..."
 echo "--------------------------------------------"
 cd $HOME
-downloadedzip2="clipboard-historyalexsaveau.dev.v44.shell-extension.zip"
-linktozip2="https://extensions.gnome.org/extension-data/clipboard-historyalexsaveau.dev.v44.shell-extension.zip"
-folder2="clipboard-history@alexsaveau.dev"
+downloadedzip1="clipboard-historyalexsaveau.dev.v41.shell-extension.zip"
+linktozip1="https://extensions.gnome.org/extension-data/clipboard-historyalexsaveau.dev.v41.shell-extension.zip"
+folder1="clipboard-history@alexsaveau.dev"
 
-mkdir $folder2
-cd $folder2
-wget $linktozip2
-unzip $downloadedzip2
-rm $downloadedzip2
+mkdir $folder1
+cd $folder1
+wget $linktozip1
+unzip $downloadedzip1
+rm $downloadedzip1
 cd $HOME
-mv $folder2 $HOME/.local/share/gnome-shell/extensions/
+mv $folder1 $HOME/.local/share/gnome-shell/extensions/
 
 echo "--------------------------------------------"
-echo "Installing desktop icons extension and disabling the broken one..."
+echo "Installing desktop icons extension..."
 echo "--------------------------------------------"
 cd $HOME
-downloadedzip2="gtk4-dingsmedius.gitlab.com.v83.shell-extension.zip"
-linktozip2="https://extensions.gnome.org/extension-data/gtk4-dingsmedius.gitlab.com.v83.shell-extension.zip"
+downloadedzip2="gtk4-dingsmedius.gitlab.com.v60.shell-extension.zip"
+linktozip2="https://extensions.gnome.org/extension-data/gtk4-dingsmedius.gitlab.com.v60.shell-extension.zip"
 folder2="gtk4-ding@smedius.gitlab.com"
 
 mkdir $folder2
@@ -49,9 +53,42 @@ cd $HOME
 mv $folder2 $HOME/.local/share/gnome-shell/extensions/
 
 echo "--------------------------------------------"
+echo "Installing corner tiling extension..."
+echo "--------------------------------------------"
+cd $HOME
+downloadedzip3="tiling-assistantleleat-on-github.v45.shell-extension.zip"
+linktozip3="https://extensions.gnome.org/extension-data/tiling-assistantleleat-on-github.v45.shell-extension.zip"
+folder3="tiling-assistant@leleat-on-github"
+
+mkdir $folder3
+cd $folder3
+wget $linktozip3
+unzip $downloadedzip3
+rm $downloadedzip3
+cd $HOME
+mv $folder3 $HOME/.local/share/gnome-shell/extensions/
+
+echo "--------------------------------------------"
+echo "Installing dash to dock..."
+echo "--------------------------------------------"
+cd $HOME
+downloadedzip4="dash-to-dockmicxgx.gmail.com.v84.shell-extension.zip"
+linktozip4="https://extensions.gnome.org/extension-data/dash-to-dockmicxgx.gmail.com.v84.shell-extension.zip"
+folder4="dash-to-dock@micxgx.gmail.com"
+
+mkdir $folder4
+cd $folder4
+wget $linktozip4
+unzip $downloadedzip4
+rm $downloadedzip4
+cd $HOME
+mv $folder4 $HOME/.local/share/gnome-shell/extensions/
+
+
+echo "--------------------------------------------"
 echo "Tweaking the file manager and ubuntu dock..."
 echo "--------------------------------------------"
-
+gsettings set org.gnome.desktop.wm.preferences button-layout "close,minimize,maximize:"
 gsettings set org.gnome.nautilus.preferences show-create-link 'true'
 gsettings set org.gnome.nautilus.preferences show-delete-permanently 'true'
 gsettings set org.gtk.Settings.FileChooser sort-directories-first 'true'
@@ -83,9 +120,11 @@ chmod +x "$HOME/Desktop/Finish_Setup.sh"
 echo '#!/bin/bash
 gnome-extensions enable clipboard-history@alexsaveau.dev
 sleep 1
-gnome-extensions disable ding@rastersoft.com
-sleep 1
 gnome-extensions enable gtk4-ding@smedius.gitlab.com
+sleep 1
+gnome-extensions enable dash-to-dock@micxgx.gmail.com
+sleep 1
+gnome-extensions enable tiling-assistant@leleat-on-github
 sleep 1
 rm "$HOME/Desktop/Finish_Setup.sh"
 ' >> "$HOME/Desktop/Finish_Setup.sh"
