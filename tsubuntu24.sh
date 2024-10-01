06518482cfb9fdfe97f2ecaa45f0e77d3773222f
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-10-01 19:13:22 +0200

    Update tsubuntu24.sh

--- tsubuntu24.sh
+++ tsubuntu24.sh
@@ -2,13 +2,13 @@
 echo "--------------------------------------------"
 echo "          Tsubuntu for Ubuntu 24.04         "
 echo "--------------------------------------------"
-sudo apt install gnome-tweaks flatpak unzip gdebi gnome-extensions-app dconf-editor libfuse2 gnome-software-plugin-snap gnome-software-plugin-flatpak gnome-software -y
+# sudo apt install gnome-tweaks flatpak unzip gdebi gnome-extensions-app dconf-editor libfuse2 gnome-software-plugin-snap gnome-software-plugin-flatpak gnome-software -y
 # libfuse2 in order for all AppImages to run
 echo "--------------------------------------------"
 echo "Adding Flathub..."
 echo "--------------------------------------------"
 
-sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+# sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 echo "--------------------------------------------"
 echo "Enabling right click > new file..."
 echo "--------------------------------------------"
@@ -103,11 +103,19 @@ echo "--------------------------------------------"
 echo "This script installed a software center app "
 echo "with support for Snap, Flatpak and native   "
 echo "packages. This makes the default snap-store "
-echo "redundant. I don't want to mess with your   "
-echo "apps so I won't touch it, but if you wish to"
-echo "remove it, type in:  snap remove snap-store "
+echo "redundant.                                  "
 echo "--------------------------------------------"
-echo "Restart your PC and then run Finish_Setup.sh"
-echo "(on your desktop)"
-echo "--------------------------------------------"
-read -p "Press Enter to close...."
+read -p "Do you want to remove the Snap Store? [y/n]: " choice
+
+
+if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+    sudo snap remove snap-store
+else
+    echo "--------------------------------------------"
+    echo "Understandable. Keeping the snap-store."
+    echo "--------------------------------------------"
+fi
+    echo "Restart your PC and then run Finish_Setup.sh"
+    echo "(on your desktop)"
+    echo "--------------------------------------------"
+    read -p "Press Enter to close...."
