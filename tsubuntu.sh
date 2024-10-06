e488553dd17516661d5227ac1db249c780f42e02
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-10-06 10:29:56 +0200

    Update tsubuntu.sh

--- tsubuntu.sh
+++ tsubuntu.sh
@@ -20,21 +20,36 @@ if [[ "$OS_ID" == "ubuntu" ]]; then
         echo "Unsupported Ubuntu version: $OS_VERSION"
         exit 1
     fi
-# Check for Debian 12
+
+# Check for Debian versions
 elif [[ "$OS_ID" == "debian" ]]; then
     if [[ "$OS_VERSION" == "12" ]]; then
         wget -q https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/tsubuntudebian.sh && chmod +x tsubuntudebian.sh && ./tsubuntudebian.sh
-# Check for Fedora 40 and 41
+    elif [[ "$OS_VERSION" == "11" ]]; then
+    	echo "This doesn't exist yet."
+        wget -q https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/tsubuntudebian11.sh && chmod +x tsubuntudebian11.sh && ./tsubuntudebian11.sh
+    elif [[ "$OS_VERSION" == "trixie" ]]; then
+    	echo "This is just a placeholder. It likely won't work that well."
+        wget -q https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/tsubuntu24.sh && chmod +x tsubuntu24.sh && ./tsubuntu24.sh
+    else
+        echo "Unsupported Debian version: $OS_VERSION"
+        exit 1
+    fi
+
+# Check for Fedora versions
 elif [[ "$OS_ID" == "fedora" ]]; then
     if [[ "$OS_VERSION" == "40" ]]; then
         wget -q https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/tsubuntufedora40.sh && chmod +x tsubuntufedora40.sh && ./tsubuntufedora40.sh
     elif [[ "$OS_VERSION" == "41" ]]; then
         wget -q https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/tsubuntufedora41.sh && chmod +x tsubuntufedora41.sh && ./tsubuntufedora41.sh
+    elif [[ "$OS_VERSION" == "42" ]]; then
+    	echo "This doesn't exist yet."
+        wget -q https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/tsubuntufedora42.sh && chmod +x tsubuntufedora42.sh && ./tsubuntufedora42.sh
     else
         echo "Unsupported Fedora version: $OS_VERSION"
         exit 1
     fi
 else
-    echo "Unsupported distro: $OS_VERSION. There is a good chance I fucked something up in the script."
-    exit 1  
+    echo "Unsupported OS: $OS_ID"
+    exit 1
 fi
