329ba00c019bab64f6f684669d0289aed83978dc
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-10-14 16:38:14 +0200

    Update tsubuntu24.sh

--- tsubuntu24.sh
+++ tsubuntu24.sh
@@ -3,7 +3,7 @@ extensionsrepo="https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/extension
 echo "┌──────────────────────────────────────────┐"
 echo "│        Tsubuntu for Ubuntu 24.04         │"
 echo "└──────────────────────────────────────────┘"
-sudo apt install gnome-tweaks flatpak unzip gdebi gnome-extensions-app dconf-editor libfuse2 gnome-software-plugin-snap gnome-software-plugin-flatpak gnome-software gufw -y
+sudo apt install gnome-tweaks flatpak unzip gdebi gnome-extensions-app dconf-editor libfuse2 gnome-software-plugin-snap gnome-software-plugin-flatpak gnome-software gufw timeshift -y
 # libfuse2 in order for all AppImages to run
 echo "┌──────────────────────────────────────────┐"
 echo "│Adding Flathub...                         │"                        
