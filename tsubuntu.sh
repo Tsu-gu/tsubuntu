eecb2024552e6153ab1900f9969cec27c0181312
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-12-21 18:16:32 +0100

    Update tsubuntu.sh

--- tsubuntu.sh
+++ tsubuntu.sh
@@ -20,6 +20,8 @@ if [[ "$OS_ID" == "ubuntu" ]]; then
         wget -q $repo/tsubuntu20.sh && chmod +x tsubuntu20.sh && ./tsubuntu20.sh
     elif [[ "$OS_VERSION" == "24.10" ]]; then
         wget -q $repo/tsubuntu2410.sh && chmod +x tsubuntu2410.sh && ./tsubuntu2410.sh
+    elif [[ "$OS_VERSION" == "25.04" ]]; then
+        wget -q $repo/tsubuntu2504.sh && chmod +x tsubuntu2504.sh && ./tsubuntu2504.sh
     else
         echo "Unsupported Ubuntu version: $OS_VERSION"
         exit 1
