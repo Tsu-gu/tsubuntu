88455200a3a2bee9deee567dddd2be3e95828d0b
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-12-22 18:04:34 +0100

    Update debiansetup.sh

--- extensions/debiansetup.sh
+++ extensions/debiansetup.sh
@@ -7,8 +7,7 @@ gsettings set org.gnome.GPaste max-displayed-history-size 10
 sleep 1
 gsettings set org.gnome.GPaste show-history '<Super>V'
 sleep 1
-gnome-extensions enable gtk4-ding@smedius.gitlab.com
-sleep 1
+gnome-extensions enable ding@rastersoft.com
 echo "Enabling the desktop icons extension..."
 gnome-extensions enable dash-to-dock@micxgx.gmail.com
 sleep 1
@@ -16,7 +15,7 @@ echo "Enabling the dash to dock extension..."
 gnome-extensions enable tiling-assistant@leleat-on-github
 sleep 1
 echo "Enabling the tiling assistant extension..."
-gnome-extensions enable appindicatorsupport@rgcjonas.gmail.com
+gnome-extensions enable ubuntu-appindicators@ubuntu.com
 sleep 1
 echo "Enabling tray icons..."
 wget -q https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/dock-setup.sh && chmod +x dock-setup.sh && ./dock-setup.sh
