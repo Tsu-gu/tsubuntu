4990932ec30ad33a9654095889a0b6a8adc94fd7
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-12-22 12:27:59 +0100

    Update tsubuntu24.sh

--- tsubuntu24.sh
+++ tsubuntu24.sh
@@ -3,8 +3,7 @@ extensionsrepo="https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/extension
 echo "┌──────────────────────────────────────────┐"
 echo "│        Tsubuntu for Ubuntu 24.04         │"
 echo "└──────────────────────────────────────────┘"
-# sudo apt install gnome-shell-extension-gpaste gnome-tweaks flatpak unzip gnome-extensions-app dconf-editor libfuse2 gnome-software-plugin-snap gnome-software-plugin-flatpak gnome-software gufw timeshift -y
-sudo apt install gnome-shell-extension-gpaste -y
+sudo apt install gnome-shell-extension-gpaste gnome-tweaks flatpak unzip gnome-extensions-app dconf-editor libfuse2 gnome-software-plugin-snap gnome-software-plugin-flatpak gnome-software gufw timeshift -y
 
 # libfuse2 in order for all AppImages to run
 echo "┌──────────────────────────────────────────┐"
