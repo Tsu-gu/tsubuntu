d0be36f171ffacfcb10b95a92a3996c25ef682ad
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-12-22 15:34:57 +0100

    Update favouriteapps.sh

--- favouriteapps.sh
+++ favouriteapps.sh
@@ -8,9 +8,11 @@ sudo snap install vlc keepassxc
 flatpak install -assumeyes flathub it.mijorus.gearlever org.qbittorrent.qBittorrent org.gimp.GIMP org.kde.kdenlive org.nicotine_plus.Nicotine 
 
 
-# filen install and integration into the desktop
 # They have a deb now, soooo.
+cd $HOME
 wget https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_amd64.deb
+sudo apt install $HOME/Filen_linux_amd64.deb -y
+sudo rm Filen_linux_amd64.deb
 
 
 # Note to self: make sure filen autostarts.
