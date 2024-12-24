106b720a5f25ebf0347c9049556b6d597c5456c0
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2024-12-24 09:35:20 +0100

    Update ubuntu2410setup.sh

--- extensions/ubuntu2410setup.sh
+++ extensions/ubuntu2410setup.sh
@@ -5,5 +5,5 @@ gsettings set org.gnome.GPaste images-support 'true'
 gsettings set org.gnome.GPaste max-displayed-history-size 10
 sleep 1
 gsettings set org.gnome.GPaste show-history '<Super>V'
-sleep 1
+
 rm "$HOME/Desktop/Finish_Setup.sh"
