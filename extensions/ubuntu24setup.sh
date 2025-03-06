e6c2fc44666f4212988424eee8ef135a3ee2c133
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2025-03-06 07:12:02 +0100

    Update ubuntu24setup.sh

--- extensions/ubuntu24setup.sh
+++ extensions/ubuntu24setup.sh
@@ -3,11 +3,7 @@ gnome-extensions enable GPaste@gnome-shell-extensions.gnome.org
 sleep 1
 gsettings set org.gnome.GPaste images-support 'true'
 gsettings set org.gnome.GPaste max-displayed-history-size 10
-sleep 1
+sleep 2
 gsettings set org.gnome.GPaste show-history '<Super>V'
-sleep 1
-gnome-extensions disable ding@rastersoft.com
-sleep 1
-gnome-extensions enable gtk4-ding@smedius.gitlab.com
 sleep 1
 rm "$HOME/Desktop/Finish_Setup.sh"
