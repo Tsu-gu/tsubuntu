fbf28544a2b2a7fd2d8c8ccd945cb069bf368229
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2025-04-19 18:43:59 +0200

    Update ubuntu2504setup.sh

--- extensions/ubuntu2504setup.sh
+++ extensions/ubuntu2504setup.sh
@@ -3,7 +3,4 @@ gnome-extensions enable GPaste@gnome-shell-extensions.gnome.org
 sleep 1
 gsettings set org.gnome.GPaste images-support 'true'
 gsettings set org.gnome.GPaste max-displayed-history-size 10
-sleep 1
-gsettings set org.gnome.GPaste show-history '<Super>V'
-
 rm "$HOME/Desktop/Finish_Setup.sh"
