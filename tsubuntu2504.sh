0db6d8011a1d83667f65aac3c05b75d9b8f244c6
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2025-04-19 18:29:34 +0200

    Update tsubuntu2504.sh

--- tsubuntu2504.sh
+++ tsubuntu2504.sh
@@ -3,9 +3,9 @@ extensionsrepo="https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/extension
 echo "┌──────────────────────────────────────────┐"
 echo "│        Tsubuntu for Ubuntu 25.04         │"
 echo "└──────────────────────────────────────────┘"
-sudo apt install gnome-shell-extension-gpaste gnome-tweaks flatpak unzip gnome-extensions-app dconf-editor libfuse2 gnome-software-plugin-snap gnome-software-plugin-flatpak gnome-software gufw timeshift -y
+sudo apt install gnome-shell-extension-gpaste gnome-tweaks flatpak gnome-shell-extensions-prefs dconf-editor libfuse2t64 gnome-software-plugin-snap gnome-software-plugin-flatpak gnome-software gufw timeshift -y
 
-# libfuse2 in order for all AppImages to run
+# libfuse2t64 in order for all AppImages to run
 echo "┌──────────────────────────────────────────┐"
 echo "│Adding Flathub...                         │"                        
 echo "└──────────────────────────────────────────┘"
