60997ea35cdeb3a1c8ba328e16e905ac35126d2e
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2025-06-05 18:16:36 +0200

    Update debiansetup.sh

--- extensions/debiansetup.sh
+++ extensions/debiansetup.sh
@@ -19,3 +19,8 @@ gnome-extensions enable ubuntu-appindicators@ubuntu.com
 sleep 1
 echo "Enabling tray icons..."
 wget -q https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/dock-setup.sh && chmod +x dock-setup.sh && ./dock-setup.sh
+
+# enable super + v to bring up the clipboard
+touch $HOME/Desktop/ReadMe.txt
+echo "Type this into the terminal after a few restarts if you want the clipboard to appear after pressing Super + V like on Windows or Plasma. Don't ask me why it can't be included in the Finish_Setup. It just doesn't work." > $HOME/Desktop/ReadMe.txt
+echo "gsettings set org.gnome.GPaste show-history '<Super>V'" > $HOME/Desktop/ReadMe.txt
