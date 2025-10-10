034ff93a67259f24ae108008c66a3411e2852e85
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2025-10-10 14:36:18 +0200

    Update ubuntu2510setup.sh

--- extensions/ubuntu2510setup.sh
+++ extensions/ubuntu2510setup.sh
@@ -7,4 +7,16 @@ rm "$HOME/Desktop/Finish_Setup.sh"
 
 touch $HOME/Desktop/ReadMe.txt
 echo "Type this into the terminal after a few restarts if you want the clipboard to appear after pressing Super + V like on Windows or Plasma. Don't ask me why it can't be included in the Finish_Setup. It just doesn't work." > $HOME/Desktop/ReadMe.txt
+echo "--------------------------------------------"
 echo "gsettings set org.gnome.GPaste show-history '<Super>V'" > $HOME/Desktop/ReadMe.txt
+echo "--------------------------------------------"
+echo "┌──────────────────────────────────────────┐"
+echo "│ Ubuntu 25.10 is using GNOME 49 which     │
+echo "│ broke Startup Applications. That's why   │
+echo "│ you can't see the app in the app menu.   │"
+echo "└──────────────────────────────────────────┘"
+echo "I recommend installing 'Ignition' from Flathub as a replacement."
+echo "--------------------------------------------"
+echo "flatpak install io.github.flattool.Ignition"
+echo "--------------------------------------------"
+
