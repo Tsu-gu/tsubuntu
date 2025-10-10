8de5aa6dde20c16c6cf9ac8d4e8ce83c75cef14c
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2025-10-10 15:30:07 +0200

    Update ubuntu2510setup.sh

--- extensions/ubuntu2510setup.sh
+++ extensions/ubuntu2510setup.sh
@@ -8,7 +8,7 @@ rm "$HOME/Desktop/Finish_Setup.sh"
 touch $HOME/Desktop/ReadMe.txt
 echo "Type this into the terminal after a few restarts if you want the clipboard to appear after pressing Super + V like on Windows or Plasma. Don't ask me why it can't be included in the Finish_Setup. It just doesn't work." > $HOME/Desktop/ReadMe.txt
 echo "--------------------------------------------"
-echo "gsettings set org.gnome.GPaste show-history '<Super>V'" > $HOME/Desktop/ReadMe.txt
+echo "gsettings set org.gnome.GPaste show-history '<Super>V'" >> $HOME/Desktop/ReadMe.txt
 echo "--------------------------------------------"
 echo "┌──────────────────────────────────────────┐"
 echo "│  Ubuntu 25.10 is using GNOME 49 which    │"
