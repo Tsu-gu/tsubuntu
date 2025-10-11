ce1f8c81b77a0b8fe2148bd2138dc72d55b43748
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2025-10-11 17:41:09 +0200

    Update tsubuntudebian13.sh

--- tsubuntudebian13.sh
+++ tsubuntudebian13.sh
@@ -94,6 +94,16 @@ else
     echo "└──────────────────────────────────────────┘"
 fi
 
+read -p "│Install Snap App Centre?            [y/n]:│" choice                                                    
+
+if [[ "$choice" == "y" || "$choice" == "Y" ]]; then
+    sudo snap install snap-store
+else
+    echo "┌──────────────────────────────────────────┐"
+    echo "│Understandable. Skipping.                 │"
+    echo "└──────────────────────────────────────────┘"
+fi
+
 echo "┌──────────────────────────────────────────┐"
 echo "│Restart your PC and then run              │"
 echo "│Finish_Setup.sh, located on your desktop  │"
