a35d4bb41f81378eee1ab99f22ee5d13d258564f
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2026-02-19 20:38:38 +0100

    Create setup script for GNOME extensions and apps
    This script sets up GNOME extensions and creates various installation scripts for recommended applications, including GPaste and Flatpak apps.

---
+++ extensions/tsubuntu2604setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+gnome-extensions enable GPaste@gnome-shell-extensions.gnome.org
+sleep 1
+gsettings set org.gnome.GPaste images-support 'true'
+gsettings set org.gnome.GPaste max-displayed-history-size 10
+
+touch $HOME/Desktop/ReadMe.txt
+echo "Type this into the terminal after a few restarts if you want the clipboard to appear after pressing Super + V like on Windows or Plasma. Don't ask me why it can't be included in the Finish_Setup. It just doesn't work." > $HOME/Desktop/ReadMe.txt
+echo "--------------------------------------------"
+echo "gsettings set org.gnome.GPaste show-history '<Super>V'" >> $HOME/Desktop/ReadMe.txt
+echo "--------------------------------------------"
+
+# InstallStartupApps.sh
+touch "$HOME/Desktop/InstallStartupApps.sh"
+echo "#!/bin/bash" > "$HOME/Desktop/InstallStartupApps.sh"
+echo "notify-send -a Tsubuntu \"Installing app, please wait a few minutes\"" >> "$HOME/Desktop/InstallStartupApps.sh"
+echo "flatpak install io.github.flattool.Ignition -y" >> "$HOME/Desktop/InstallStartupApps.sh"
+echo "notify-send -a Tsubuntu \"Ignition installed\"" >> "$HOME/Desktop/InstallStartupApps.sh"
+chmod +x "$HOME/Desktop/InstallStartupApps.sh"
+
+# InstallFlatpakAppStore.sh
+touch "$HOME/Desktop/InstallFlatpakAppStore.sh"
+echo "#!/bin/bash" > "$HOME/Desktop/InstallFlatpakAppStore.sh"
+echo "notify-send -a Tsubuntu \"Installing app, please wait a few minutes\"" >> "$HOME/Desktop/InstallFlatpakAppStore.sh"
+echo "flatpak install io.github.kolunmi.Bazaar -y" >> "$HOME/Desktop/InstallFlatpakAppStore.sh"
+echo "notify-send -a Tsubuntu \"Bazaar installed\"" >> "$HOME/Desktop/InstallFlatpakAppStore.sh"
+chmod +x "$HOME/Desktop/InstallFlatpakAppStore.sh"
+
+# InstallAppImageIntegrator.sh
+touch "$HOME/Desktop/InstallAppImageIntegrator.sh"
+echo "#!/bin/bash" > "$HOME/Desktop/InstallAppImageIntegrator.sh"
+echo "notify-send -a Tsubuntu \"Installing app, please wait a few minutes\"" >> "$HOME/Desktop/InstallAppImageIntegrator.sh"
+echo "flatpak install it.mijorus.gearlever -y" >> "$HOME/Desktop/InstallAppImageIntegrator.sh"
+echo "notify-send -a Tsubuntu \"GearLever installed\"" >> "$HOME/Desktop/InstallAppImageIntegrator.sh"
+chmod +x "$HOME/Desktop/InstallAppImageIntegrator.sh"
+
+# InstallMenuEditor.sh
+touch "$HOME/Desktop/InstallMenuEditor.sh"
+echo "notify-send -a Tsubuntu \"Installing app, please wait a few minutes\"" >> "$HOME/Desktop/InstallMenuEditor.sh"
+echo "flatpak install page.codeberg.libre_menu_editor.LibreMenuEditor -y" >> "$HOME/Desktop/InstallMenuEditor.sh"
+echo "notify-send -a Tsubuntu \"Main Menu installed\"" >> "$HOME/Desktop/InstallMenuEditor.sh"
+chmod +x "$HOME/Desktop/InstallMenuEditor.sh"
+
+# InstallAllRecommended
+touch "$HOME/Desktop/InstallAllRecommended.sh"
+echo "notify-send -a Tsubuntu \"Installing recommended apps, please wait a few minutes\"" >> "$HOME/Desktop/InstallAllRecommended.sh"
+echo "flatpak install io.github.flattool.Ignition page.codeberg.libre_menu_editor.LibreMenuEditor it.mijorus.gearlever io.github.kolunmi.Bazaar -y" >> "$HOME/Desktop/InstallAllRecommended.sh"
+echo "notify-send -a Tsubuntu \"Recommended apps installed\"" >> "$HOME/Desktop/InstallAllRecommended.sh"
+chmod +x "$HOME/Desktop/InstallAllRecommended.sh"
