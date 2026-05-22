813d1a1096e829392f04cd9c3169735b4b2b8c23
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2026-05-22 20:18:50 +0200

    Update tsubuntu2604.sh

--- tsubuntu2604.sh
+++ tsubuntu2604.sh
@@ -3,7 +3,7 @@ extensionsrepo="https://raw.githubusercontent.com/Tsu-gu/tsubuntu/main/extension
 echo "┌──────────────────────────────────────────┐"
 echo "│     Tsubuntu for Ubuntu 26.04 LTS        │"
 echo "└──────────────────────────────────────────┘"
-sudo apt install gir1.2-gda-5.0 gir1.2-gsound-1.0 gnome-tweaks flatpak dconf-editor libfuse2t64 gnome-shell-extension-manager gufw timeshift software-properties-gtk -y
+sudo apt install gir1.2-gda-5.0 gir1.2-gsound-1.0 gnome-tweaks flatpak dconf-editor libfuse2t64 gnome-shell-extension-manager gufw timeshift software-properties-gtk gst-audio-thumbnailer gst-video-thumbnailer -y
 
 # libfuse2t64 in order for all AppImages to run. Many AppImages are built depending on this old version. Like it or not you either have it installed or half of them won't run. 
 # software-properties-gtk to return the driver manager and Software & Updates
