aca1285fef0d60a4ad32de51dfed9c4156494dc6
Tsu-gu <108401269+Tsu-gu@users.noreply.github.com>
2026-06-13 16:09:40 +0200

    Update ubuntu2604setup.sh

--- extensions/ubuntu2604setup.sh
+++ extensions/ubuntu2604setup.sh
@@ -10,6 +10,6 @@ dconf write /org/gnome/shell/extensions/copyous/history-length 15
 # InstallAllRecommended
 touch "$HOME/Desktop/InstallAllRecommended.sh"
 echo "notify-send -a Tsubuntu \"Installing recommended apps, please wait a few minutes\"" >> "$HOME/Desktop/InstallAllRecommended.sh"
-echo "flatpak install io.github.flattool.Ignition page.codeberg.libre_menu_editor.LibreMenuEditor it.mijorus.gearlever io.github.kolunmi.Bazaar -y" >> "$HOME/Desktop/InstallAllRecommended.sh"
+echo "flatpak install io.github.flattool.Ignition page.codeberg.libre_menu_editor.LibreMenuEditor it.mijorus.gearlever -y" >> "$HOME/Desktop/InstallAllRecommended.sh"
 echo "notify-send -a Tsubuntu \"Recommended apps installed\"" >> "$HOME/Desktop/InstallAllRecommended.sh"
 chmod +x "$HOME/Desktop/InstallAllRecommended.sh"
